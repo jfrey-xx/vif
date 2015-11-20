7db0c1a8c86a410d052e761052dc2bbe11d15b55
jfrey <nomhad@gmail.com>
2015-11-19 20:26:37 -0500

    switching dk2 to symbolic link

--- OculusDk2/textWord.java
+++ OculusDk2/textWord.java
@@ -1,18 +1,1 @@
-/**
- 
- Atomic unit for text.
- 
- TODO: handle picking at word level. 
- 
- */
-
-import geomerative.*;
-
-class textWord {
-  RGroup group;
-  String text;
-  textWord(RGroup group, String text) {
-    this.group =group;
-    this.text = text;
-  }
-}
+../blocks/textWord.java
